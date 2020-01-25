8acc9605fda7c19408e77a13a3a1c8b002444989
Legend <maesabroso@gmail.com>
2020-01-25 17:02:20 -0600

    search working

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-$('#test').on('click', function(){
-    var searchInput = $('#searchInput').val();
-    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchInput;
-     $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response){
-        console.log(response);
-        var results = response.drinks;
-        var drinkcard = $('#fadeCardFather');
+$('#cocktail').on('click', function(){
+  var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail";
+   $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response){
+      console.log(response);
 
-        
 
-        for(var i = 0; i < results.length; i++){
+      var results = response.drinks;
+      var drinkcard = $('#fadeCardFather');
 
+      
+      for(var i = 0; i < 4; i++){
+          //randomize a number variable 
+          var randomDrink = 1 + Math.floor(Math.random() * 100);
           var searchDiv = $('<div>');
           searchDiv.attr('class', 'uk-card uk-card-default uk-card-body fadeCard');
-          var drinkName = $('<h3>').text(results[i].strDrink);
+          var drinkName = $('<h3>').text(results[randomDrink].strDrink);
           drinkName.attr('class', 'uk-card-title');
           var drinkImg = $('<img>');
-          drinkImg.attr('src', results[i].strDrinkThumb);
+          drinkImg.attr('src', results[randomDrink].strDrinkThumb);
           drinkImg.attr('class', 'uk-img')
           var drinkVid = $('<button>');
           drinkVid.attr('class', 'uk-button uk-button-secondary uk-button-small');
@@ -32,19 +33,21 @@ $('#test').on('click', function(){
           var spanInstIcon = $('<span>').attr('class', "fas fa-glass-martini-alt");
           drinkVid.append(spanVidIcon);
           drinkInst.append(spanInstIcon);
-          // var instBtn = $('<button>').text('Show Instructions');
-          // var videosBtn = $('<button>').text('Videos');
           searchDiv.append(drinkName, drinkImg, drinkVid, drinkInst);
           drinkcard.prepend(searchDiv);
+          $('#modal-div').prepend(drinkcard);
 
         }
 
 
+    })
 
+  })
+      
+    
+    
+  
 
-    });
-
-});
 
 
 
@@ -55,7 +58,7 @@ $('#test').on('click', function(){
 //         method: "GET"
 //       }).then(function(response){
 //         console.log(response);
-    
+  
 //         var randomDiv = $('#randomDiv');
 //         var drinkName = $('<h1>').text(response.drinks[0].strDrink);
 //         console.log(drinkName);
@@ -66,4 +69,3 @@ $('#test').on('click', function(){
 //     });
 
 // });
-
